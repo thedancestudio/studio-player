bbf9735ae4e4fdf36b7b4b90274f2ec47d098b37
thedancestudio <thedancestudio.art@gmail.com>
2026-07-05 18:41:51 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+import { renderSidebar } from "./ui/sidebar.js";
+import { renderTopBar } from "./ui/topbar.js";
+import { renderPlayer } from "./ui/playerbar.js";
 
+renderSidebar();
+renderTopBar();
+renderPlayer();
+
+document.querySelector("#content").innerHTML = `
+    <h1>Studio Player</h1>
+    <p>Welcome.</p>
+`;
