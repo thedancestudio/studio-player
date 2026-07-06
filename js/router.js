1cd95bc1c9ca7c6accf7ec1669f32ac43db87267
thedancestudio <thedancestudio.art@gmail.com>
2026-07-05 19:06:57 -0500

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,1 +1,27 @@
+import { ProductionsPage } from "./pages/productions.js";
+import { LibraryPage } from "./pages/library.js";
+import { QueuePage } from "./pages/queue.js";
+import { SearchPage } from "./pages/search.js";
+import { SettingsPage } from "./pages/settings.js";
 
+const routes = {
+
+    "#/productions": ProductionsPage,
+
+    "#/library": LibraryPage,
+
+    "#/queue": QueuePage,
+
+    "#/search": SearchPage,
+
+    "#/settings": SettingsPage
+
+};
+
+export function getCurrentPage() {
+
+    const hash = location.hash || "#/productions";
+
+    return routes[hash] || ProductionsPage;
+
+}
