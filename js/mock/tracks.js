5618707694681d1dc08bf35e82d7f7a86fac1c85
thedancestudio <thedancestudio.art@gmail.com>
2026-07-06 12:55:37 -0500

    Create tracks.js

---
+++ js/mock/tracks.js
@@ -0,0 +1,30 @@
+export const tracks = [
+    {
+        id: 1,
+        title: "Opening Number",
+        artist: "Studio Orchestra",
+        album: "Spring Recital 2027",
+        duration: "2:31"
+    },
+    {
+        id: 2,
+        title: "Waltz",
+        artist: "Studio Orchestra",
+        album: "Spring Recital 2027",
+        duration: "3:08"
+    },
+    {
+        id: 3,
+        title: "Tap Finale",
+        artist: "Studio Orchestra",
+        album: "Spring Recital 2027",
+        duration: "2:44"
+    },
+    {
+        id: 4,
+        title: "Curtain Call",
+        artist: "Studio Orchestra",
+        album: "Spring Recital 2027",
+        duration: "1:56"
+    }
+];
