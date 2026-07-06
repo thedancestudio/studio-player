faab255a1bf03a6f1fb8ec8a0bdf3f39b43107bd
thedancestudio <thedancestudio.art@gmail.com>
2026-07-06 18:09:51 -0500

    Update App.js

--- js/app/App.js
+++ js/app/App.js
@@ -5,12 +5,14 @@ import { getCurrentPage } from "../router.js";
 export class App {
 
     constructor() {
+
         this.sidebar = document.getElementById("sidebar");
         this.topbar = document.getElementById("topbar");
         this.content = document.getElementById("content");
         this.player = document.getElementById("player");
 
         this.route = location.hash || "#/library";
+
     }
 
     start() {
@@ -19,25 +21,30 @@ export class App {
             location.hash = "#/library";
         }
 
-        this.renderShell();
-        this.renderPage();
+        this.render();
 
         window.addEventListener("hashchange", () => {
+
             this.route = location.hash;
-            this.renderPage();
+
+            this.render();
+
         });
 
     }
 
-    renderShell() {
-        this.sidebar.innerHTML = Sidebar(this.route);
-        this.topbar.innerHTML = TopBar();
-    }
+    render() {
 
-    renderPage() {
-        const Page = getCurrentPage();
-        this.content.innerHTML = Page();
+        const page = getCurrentPage();
+
         this.sidebar.innerHTML = Sidebar(this.route);
+
+        this.topbar.innerHTML = TopBar(page);
+
+        this.content.innerHTML = page.content;
+
+        this.player.innerHTML = "";
+
     }
 
 }
