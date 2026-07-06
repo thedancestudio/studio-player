ccb427a9b42e66ecc3578e79cd1fe7c670afdb46
thedancestudio <thedancestudio.art@gmail.com>
2026-07-06 12:53:54 -0500

    Update navigation.js

--- js/data/navigation.js
+++ js/data/navigation.js
@@ -1,42 +1,29 @@
 export const navigation = [
-
-    {
-        id: "productions",
-        label: "Productions",
-        icon: "theater_comedy",
-        route: "#/productions"
-    },
-
     {
         id: "library",
         label: "Library",
         icon: "library_music",
         route: "#/library"
     },
-
+    {
+        id: "playlists",
+        label: "Playlists",
+        icon: "playlist_play",
+        route: "#/playlists"
+    },
     {
         id: "queue",
         label: "Queue",
         icon: "queue_music",
         route: "#/queue"
-    },
-
-    {
-        id: "search",
-        label: "Search",
-        icon: "search",
-        route: "#/search"
     }
-
 ];
 
 export const settingsNavigation = [
-
     {
         id: "settings",
         label: "Settings",
         icon: "settings",
         route: "#/settings"
     }
-
 ];
