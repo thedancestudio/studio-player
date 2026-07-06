783ac86a96f3987fab3bcfe3cf715ce2cec781a6
thedancestudio <thedancestudio.art@gmail.com>
2026-07-06 18:10:47 -0500

    Create albums.js

---
+++ js/mock/albums.js
@@ -0,0 +1,33 @@
+export const albums = [
+
+    {
+        id: 1,
+        title: "Spring Recital 2027",
+        artist: "Studio Orchestra"
+    },
+
+    {
+        id: 2,
+        title: "Christmas Show",
+        artist: "Studio Orchestra"
+    },
+
+    {
+        id: 3,
+        title: "Nutcracker",
+        artist: "Studio Orchestra"
+    },
+
+    {
+        id: 4,
+        title: "Summer Intensive",
+        artist: "Studio Orchestra"
+    },
+
+    {
+        id: 5,
+        title: "Competition Mixes",
+        artist: "Studio Orchestra"
+    }
+
+];
