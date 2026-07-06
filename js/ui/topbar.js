330be44fb0e3479b6519291018aa1fd3856cba13
thedancestudio <thedancestudio.art@gmail.com>
2026-07-06 13:22:36 -0500

    Update topbar.js

--- js/ui/topbar.js
+++ js/ui/topbar.js
@@ -1,52 +1,13 @@
 export function TopBar() {
 
     return `
-
         <div class="topbar-title">
-
-            <h1>Studio Player</h1>
-
-            <small>The Dance Studio</small>
-
+            <h1>Library</h1>
+            <small>Browse your music</small>
         </div>
 
-        <div class="topbar-right">
-
-            <label class="search">
-
-                <span class="material-symbols-rounded">
-                    search
-                </span>
-
-                <input
-                    type="text"
-                    placeholder="Search..."
-                >
-
-            </label>
-
-            <button class="icon-button">
-
-                <span class="material-symbols-rounded">
-
-                    sync
-
-                </span>
-
-            </button>
-
-            <button class="icon-button">
-
-                <span class="material-symbols-rounded">
-
-                    settings
-
-                </span>
-
-            </button>
-
+        <div class="topbar-actions">
+            <span class="material-symbols-rounded">search</span>
         </div>
-
     `;
-
 }
