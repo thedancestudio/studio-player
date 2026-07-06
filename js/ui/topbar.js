b4441f66c42cb7502a61808ff4328f8345bd48e2
thedancestudio <thedancestudio.art@gmail.com>
2026-07-05 20:26:12 -0500

    Update topbar.js

--- js/ui/topbar.js
+++ js/ui/topbar.js
@@ -1,5 +1,52 @@
-export function renderTopBar(){
+export function TopBar() {
 
-    document.querySelector("#topbar").innerHTML = "";
+    return `
+
+        <div class="topbar-title">
+
+            <h1>Studio Player</h1>
+
+            <small>The Dance Studio</small>
+
+        </div>
+
+        <div class="topbar-right">
+
+            <label class="search">
+
+                <span class="material-symbols-rounded">
+                    search
+                </span>
+
+                <input
+                    type="text"
+                    placeholder="Search..."
+                >
+
+            </label>
+
+            <button class="icon-button">
+
+                <span class="material-symbols-rounded">
+
+                    sync
+
+                </span>
+
+            </button>
+
+            <button class="icon-button">
+
+                <span class="material-symbols-rounded">
+
+                    settings
+
+                </span>
+
+            </button>
+
+        </div>
+
+    `;
 
 }
