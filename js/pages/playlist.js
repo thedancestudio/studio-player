607abeb5721e4c37b019352ceb49951e301e4ea5
thedancestudio <thedancestudio.art@gmail.com>
2026-07-06 13:23:53 -0500

    Update and rename productions.js to playlist.js

---
+++ js/pages/playlist.js
@@ -0,0 +1,8 @@
+export function PlaylistsPage() {
+    return `
+        <div class="placeholder">
+            <h1>Playlists</h1>
+            <p>Playlist editor coming next.</p>
+        </div>
+    `;
+}
