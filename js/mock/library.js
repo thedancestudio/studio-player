1d55937a5b2d6160fb7947f78f8912ed0fb29b78
thedancestudio <thedancestudio.art@gmail.com>
2026-07-07 07:47:59 -0500

    Create library.js

---
+++ js/mock/library.js
@@ -0,0 +1,69 @@
+export const library = [
+
+    {
+        id: "SPR-001",
+        title: "Opening Number",
+        artist: "Studio Orchestra",
+        album: "Spring Recital 2027",
+        artwork: "",
+        duration: "2:31",
+        track: 1,
+        driveId: ""
+    },
+
+    {
+        id: "SPR-002",
+        title: "Ballet Suite",
+        artist: "Studio Orchestra",
+        album: "Spring Recital 2027",
+        artwork: "",
+        duration: "3:48",
+        track: 2,
+        driveId: ""
+    },
+
+    {
+        id: "SPR-003",
+        title: "Tap Finale",
+        artist: "Studio Orchestra",
+        album: "Spring Recital 2027",
+        artwork: "",
+        duration: "2:12",
+        track: 3,
+        driveId: ""
+    },
+
+    {
+        id: "XMAS-001",
+        title: "Snow Waltz",
+        artist: "Studio Orchestra",
+        album: "Christmas Show",
+        artwork: "",
+        duration: "2:47",
+        track: 1,
+        driveId: ""
+    },
+
+    {
+        id: "XMAS-002",
+        title: "Toy Soldiers",
+        artist: "Studio Orchestra",
+        album: "Christmas Show",
+        artwork: "",
+        duration: "3:05",
+        track: 2,
+        driveId: ""
+    },
+
+    {
+        id: "COMP-001",
+        title: "Lyrical Solo",
+        artist: "Studio Orchestra",
+        album: "Competition Music",
+        artwork: "",
+        duration: "2:41",
+        track: 1,
+        driveId: ""
+    }
+
+];
